d8a25a546cdf49c4422187c7d613773a0f4a3222
Tiffany Burke <106708206+tiffcburke@users.noreply.github.com>
2023-12-16 11:52:30 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,79 +1,77 @@
 const express = require('express');
+const app = express();
 const path = require('path');
-const { v4: uuid } = require('uuid');
-uuid();
-app = express();
+const mongoose = require("mongoose");
+const methodOverride = require('method-override');
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
+const Book = require('./models/book');
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://127.0.0.1:27017/alphaBookClub', { useNewUrlParser: true })
+    .then(() => {
+        console.log("Mongo connection open!")
+    })
+    .catch(err => {
+        console.log("Oh no mongo connection error!")
+        console.log(err)
+    })
+
+
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
+app.get('/books', async (req, res) => {
+    const { recommendedBy } = req.query;
+    if (recommendedBy) {
+        const books = await Book.find({ recommendedBy: recommendedBy });
+        res.render('books/index.ejs', { books, recommendedBy })
+    } else {
+        const books = await Book.find({});
+        res.render('books/index.ejs', { books, recommendedBy: '' })
 
-let books = [
-    {
-        username: "kcasario",
-        book: "Tress of the Emerald Sea"
-    },
-    {
-        username: "kelgirl",
-        book: "Montanan Book"
-    },
-    {
-        username: "tia",
-        book: "My little pony"
-    },
-    {
-        username: "jamesK",
-        book: "Engineering book"
     }
-]
-
-let newNotes = [
-    {
-        id: uuid(),
-        title: 'Page 10',
-        comment: "wow I cant believe that happened so crazy like wow"
-    },
-    {
-        id: uuid(),
-        title: 'Page 25',
-        comment: "Oh I see what happened here."
-    },
-    {
-        id: uuid(),
-        title: 'Page 150',
-        comment: "I knew this was going to happen"
-    },
-];
-
-
-
-//home page, displaying book choices
-app.get("/books", (req, res) => {
-    // console.log("does this work?")
-    res.render("books/index.ejs", { books, newNotes })
 })
 
-
-//note page where you can create notes & questions on the book you are reading.
-app.get("/books/note", (req, res) => {
-    res.render("books/note.ejs")
-})
-app.post('/books', (req, res) => {
-    const { title, comment } = req.body;
-    newNotes.push({ title, comment, id: uuid() })
-    res.redirect('/books');
+app.get('/books', async (req, res) => {
+    const books = await Book.find({});
+    res.render('books/index.ejs', { books })
 })
 
-//Edit a comment
-app.patch('/books/:id', (req, res) => {
+app.get('/books/new', (req, res) => {
+    res.render('books/new')
+})
+
+app.post('/books', async (req, res) => {
+    const newBook = new Book(req.body);
+    await newBook.save();
+    console.log(newBook);
+    res.redirect(`/books/${newBook._id}`)
+})
+
+app.get('/books/:id/edit', async (req, res) => {
     const { id } = req.params;
-    const newCommentText = req.body.comment;
-    const foundComment = newNotes.find(c => c.id === id);
-    foundComment.comment = newCommentText;
+    const book = await Book.findById(id);
+    res.render('books/edit', { book });
+})
+
+app.put('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    const book = await Book.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/books/${book._id}`);
+})
+
+app.get('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    res.render('books/show.ejs', { book })
+})
+
+app.delete('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    const deleteBook = await Book.findByIdAndDelete(id);
     res.redirect('/books');
 })
 
@@ -87,5 +85,5 @@ app.patch('/books/:id', (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("listening on 3000")
+    console.log("App is listening on port 3000!")
 })
